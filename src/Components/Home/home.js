ae535b336f88adb054f21d5920070462a2c274f1
Tom Strother <tom_strother@yahoo.com>
2020-06-12 12:40:19 -0500

    home page up

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -1,7 +1,22 @@
 import React from "react";
+import "./home.css";
 
 const HomePage = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <section className="homebkgd BGDimg ">
+        <section className="AccessSect ">
+          <section className="CreateAccSect ">
+            <div className="createAccBtn">Don't have an account?</div>
+            <div className="createAccHyperlink space4px">Create Account</div>
+          </section>
+          <section className="signInBtn">
+            <button type="button">Sign In</button>
+          </section>
+        </section>
+      </section>
+    </>
+  );
 };
 
 export default HomePage;
