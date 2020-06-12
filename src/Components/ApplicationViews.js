2eca2f8b9790da872eabdf90f716f559817fd66a
Tom Strother <tom_strother@yahoo.com>
2020-06-12 16:15:56 -0500

    stopping point

--- src/Components/ApplicationViews.js
+++ src/Components/ApplicationViews.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import React from "react";
 import HomePage from "./Home/home";
 import Login from "./Auth/login";
+import UserTours from "./UserTours/userTours";
 
 const ApplicationViews = (props) => {
   const hasUser = props.hasUser;
@@ -20,6 +21,12 @@ const ApplicationViews = (props) => {
         path="/login"
         render={(props) => {
           return <Login setUser={setUser} {...props} />;
+        }}
+      />
+      <Route
+        path="/tours"
+        render={(props) => {
+          return <UserTours setUser={setUser} {...props} />;
         }}
       />
     </Router>
---
+++ src/Components/UserTours/userTours.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./userTours.css";
+
+const UserTours = () => {
+  return (
+    <>
+      <section></section>
+    </>
+  );
+};
+
+export default UserTours;
