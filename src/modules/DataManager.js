601e0854904d5a1e1a4ea6063fb88d10417f6f2f
Tom Strother <tom_strother@yahoo.com>
2020-06-15 14:09:02 -0500

    tour creation page updating to database

---
+++ src/modules/DataManager.js
@@ -0,0 +1,35 @@
+const remoteURL = "http://localhost:5002";
+
+export default {
+  get(id, resource, expand) {
+    return fetch(`${remoteURL}/${resource}/${id}${expand}`).then((result) =>
+      result.json()
+    );
+  },
+  getAll(resource) {
+    return fetch(`${remoteURL}/${resource}`).then((result) => result.json());
+  },
+  delete(id) {
+    return fetch(`${remoteURL}/products/${id}`, {
+      method: "DELETE",
+    }).then((result) => result.json());
+  },
+  post(newMedia) {
+    return fetch(`${remoteURL}/photoAlbums/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newMedia),
+    }).then((data) => data.json());
+  },
+  update(editedMedia) {
+    return fetch(`${remoteURL}/products/${editedMedia.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(editedMedia),
+    }).then((data) => data.json());
+  },
+};
