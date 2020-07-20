68f1854e56b16c54cb7e578a84fd451b5b950858
Tom Strother <tom_strother@yahoo.com>
2020-07-20 07:29:51 -0500

    added buttons in tour viewer to a dropwdown

--- src/Components/tourViewer/tourViewer.js
+++ src/Components/tourViewer/tourViewer.js
@@ -10,8 +10,10 @@ const TourViewer = (props) => {
   const [tourData, setTourData] = useState([]);
   const [newImage, setnewImage] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
+  const [isOpen2, setIsOpen2] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
+  const toggle2 = () => setIsOpen2(!isOpen2);
 
   const handleFieldChange = (evt) => {
     ReactGa.event({
@@ -31,29 +33,52 @@ const TourViewer = (props) => {
 
   return (
     <Container>
-      <div>
+      <div className="">
         <section>
           <a-scene class="aframebox1" embedded>
             <a-sky src={newImage}></a-sky>
           </a-scene>
-          <section className="tourImageLinks1 move topright">
+          {/* <section className="tourImageLinks1 bottomright">
             <ImagesCard handleFieldChange={handleFieldChange} {...props} />
-          </section>
+          </section> */}
           <h6 className="tourTitle tourNameHeader move">{tourData.title}</h6>
-          <div className="topleft tourImageLinks1 move ">
-            <Button
-              color="primary"
-              onClick={toggle}
-              style={{ marginBottom: "1rem" }}
-            >
-              Tour Description
-            </Button>
-            <Collapse isOpen={isOpen}>
-              <Card>
-                <CardBody>{tourData.description}</CardBody>
-              </Card>
-            </Collapse>
-          </div>
+          <section className="tourMenuBtns topleft tourImageLinks1 move">
+            <div className="">
+              <Button
+                color="primary"
+                onClick={toggle}
+                style={{ marginBottom: "1rem" }}
+              >
+                Tour Description
+              </Button>
+              <Collapse isOpen={isOpen}>
+                <Card>
+                  <CardBody>{tourData.description}</CardBody>
+                </Card>
+              </Collapse>
+            </div>
+            <div className="">
+              <Button
+                color="primary"
+                onClick={toggle2}
+                style={{ marginBottom: "1rem" }}
+              >
+                VR Rooms
+              </Button>
+              <Collapse isOpen={isOpen2}>
+                <section className="">
+                  <Card>
+                    <CardBody>
+                      <ImagesCard
+                        handleFieldChange={handleFieldChange}
+                        {...props}
+                      />
+                    </CardBody>
+                  </Card>
+                </section>
+              </Collapse>
+            </div>
+          </section>
         </section>
       </div>
     </Container>
